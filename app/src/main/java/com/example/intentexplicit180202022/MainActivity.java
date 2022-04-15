a9c5874e929f2d3d472e6552346c0b5eefcc660a
Phat <phatdroid94@gmail.com>
2022-04-15 18:00:17 +0700

    send string

--- app/src/main/java/com/example/intentexplicit180202022/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit180202022/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.intentexplicit180202022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
         mBinding.buttonString.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("text","hello");
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/intentexplicit180202022/MainActivity2.java
+++ app/src/main/java/com/example/intentexplicit180202022/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.intentexplicit180202022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        // 1 : Nhận kiểu string
+        Intent intent = getIntent();
+
+        if (intent != null){
+            String text = intent.getStringExtra("text");
+            Log.d("BBB",text);
+        }
     }
 }
